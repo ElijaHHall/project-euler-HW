d122d24ed74394d231be73e896f81e5868dd15ac
ElijahHHall <ehall851@gmail.com>
2018-05-16 10:00:03 -0600

    struggling with problem 2

--- project_euler.py
+++ project_euler.py
@@ -1,18 +1,26 @@
-num = [3,5]
-max = 1000
+# 1. # num = [3,5]
+# max = 1000
 
-result = 0
-for num in num:
-	for i in range(1,1000):
-		if num*i < max:
-			result += num*i
-print(result)
+# result = 0
+# for num in num:
+# 	for i in range(1,1000):
+# 		if num*i < max:
+# 			result += num*i
+# print(result)
 
-result = 0
-for i in range(0,1000):
-	if i%3 == 0 or i%5 == 0:
-		result += i
-		
-print(result)
+# result = 0
+# for i in range(0,1000):
+# 	if i%3 == 0 or i%5 == 0:
+# 		result += i
+
+# print(result)
 
 
+def SubFib(startNumber, endNumber):
+	for cur in SubFib():
+		if cur > endNumber: return
+		if cur >= startNumber:
+			yield cur
+
+for i in SubFib(10, 200):
+	print(i)
